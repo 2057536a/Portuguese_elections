1767d2c4f8d6abc438ef72a68cefd28c529b6238
2057536a <ioannis.atgr@gmail.com>
2020-08-26 19:08:37 +0100

    Adding the py files instead pof notebooks

--- random_forrest_regression.py
+++ random_forrest_regression.py
@@ -4,10 +4,12 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
-# IMPORTING THE DATASET
+
+
+# IMPORTING THE DATA
 dataset = pd.read_csv("ElectionData.csv")
 
 dataset = dataset.drop("TimeElapsed", axis=1)
@@ -16,9 +18,6 @@ dataset = dataset.drop("time", axis=1)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-print(X)
-print(y)
-
 # ENCODING CATEGORICAL DATA
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0,19])], remainder='passthrough')
 
@@ -26,20 +25,15 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0,19])], rema
 # to save the output to X as a numpy array because this will be expected from the machine learning algo
 X = np.array(ct.fit_transform(X))
 
-# SPLITTING INTO TRAINING AND TEST SET
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+# SPLITTING THE DATA
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# TRAIN THE MULTIPLE REGRESSION MODEL
-regressor = LinearRegression()
+# TRAIN THE RANDOM FORREST REGRESSION MODEL
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 regressor.fit(X_train, y_train)
 
 # PREDICTING TEST SET RESULTS
 y_pred = regressor.predict(X_test)
 
-print(regressor.coef_)
-print(regressor.intercept_)
+# EVALUATING THE SVR PERFORMANCE
 print(r2_score(y_test, y_pred))
-
-for i in range(len(y_pred)):
-    if y_test[i] >= 2:
-        print("Predicted: {}/ Actual: {}".format(y_pred[i], y_test[i]))
--- support_vector_regression.py
+++ support_vector_regression.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
 from sklearn.metrics import r2_score
 
-# IMPORTING THE DATASET
+# IMPORTING THE DATA
 dataset = pd.read_csv("ElectionData.csv")
 
 dataset = dataset.drop("TimeElapsed", axis=1)
@@ -16,9 +17,6 @@ dataset = dataset.drop("time", axis=1)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-print(X)
-print(y)
-
 # ENCODING CATEGORICAL DATA
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0,19])], remainder='passthrough')
 
@@ -26,20 +24,26 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0,19])], rema
 # to save the output to X as a numpy array because this will be expected from the machine learning algo
 X = np.array(ct.fit_transform(X))
 
-# SPLITTING INTO TRAINING AND TEST SET
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+# SPLITTING THE DATA
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# TRAIN THE MULTIPLE REGRESSION MODEL
-regressor = LinearRegression()
+# FEATURE SCALING
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train[:, 42:] = sc_X.fit_transform(X_train[:, 42:])
+X_test[:, 42:] = sc_X.transform(X_test[:, 42:])
+
+y_train = sc_y.fit_transform(y_train)
+y_test = sc_y.transform(y_test)
+
+# TRAIN THE SVR MODEL
+regressor = SVR(kernel = 'rbf') # rfb = radial basis function (non linear function)
 regressor.fit(X_train, y_train)
 
 # PREDICTING TEST SET RESULTS
-y_pred = regressor.predict(X_test)
+y_pred = sc_y.inverse_transform(regressor.predict(X_test))
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
-print(regressor.coef_)
-print(regressor.intercept_)
-print(r2_score(y_test, y_pred))
-
-for i in range(len(y_pred)):
-    if y_test[i] >= 2:
-        print("Predicted: {}/ Actual: {}".format(y_pred[i], y_test[i]))
+# EVALUATING THE SVR PERFORMANCE
+print(r2_score(sc_y.inverse_transform(y_test), y_pred))
